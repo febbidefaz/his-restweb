23d7aad9dc120a686f1f42f70d8bfea6d5d3ceb5
febbidefaz <febbi_dea@ymail.com>
2026-05-11 12:39:29 +0700

    update febbi

--- src/main/java/com/dev/generik/entities/HaloRSA/QuePx.java
+++ src/main/java/com/dev/generik/entities/HaloRSA/QuePx.java
@@ -4,8 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter 
+@Setter
 @Table(name = "WSQUEPX")
 public class QuePx {
 	/*
@@ -31,96 +35,6 @@ public class QuePx {
 	private String dokter;
 	private String fotoOL;
 
-	public QuePx() {
-		super();
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public Long getDocid() {
-		return docid;
-	}
-
-	public void setDocid(Long docid) {
-		this.docid = docid;
-	}
-
-	public Long getJp() {
-		return jp;
-	}
-
-	public void setJp(Long jp) {
-		this.jp = jp;
-	}
-
-	public Long getAllpx() {
-		return allpx;
-	}
-
-	public void setAllpx(Long allpx) {
-		this.allpx = allpx;
-	}
-
-	public Long getFinishpx() {
-		return finishpx;
-	}
-
-	public void setFinishpx(Long finishpx) {
-		this.finishpx = finishpx;
-	}
-
-	public Long getQuepx() {
-		return quepx;
-	}
-
-	public void setQuepx(Long quepx) {
-		this.quepx = quepx;
-	}
-
-	public Long getNumberpx() {
-		return numberpx;
-	}
-
-	public void setNumberpx(Long numberpx) {
-		this.numberpx = numberpx;
-	}
-
-	public String getPoli() {
-		return poli;
-	}
-
-	public void setPoli(String poli) {
-		this.poli = poli;
-	}
-
-	public Long getIdpoli() {
-		return idpoli;
-	}
-
-	public void setIdpoli(Long idpoli) {
-		this.idpoli = idpoli;
-	}
-
-	public String getDokter() {
-		return dokter;
-	}
-
-	public void setDokter(String dokter) {
-		this.dokter = dokter;
-	}
-
-	public String getFotoOL() {
-		return fotoOL;
-	}
-
-	public void setFotoOL(String fotoOL) {
-		this.fotoOL = fotoOL;
-	}
+	
 
 }
